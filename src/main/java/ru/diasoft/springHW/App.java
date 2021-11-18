7dc8e5db7333fb591dba913958414fa764711826
anepretimovdiasoft <anepretimov@diasoft.ru>
2021-11-18 17:36:29 +0300

    comment table
    added comment table to liquibase

--- src/main/java/ru/diasoft/springHW/App.java
+++ src/main/java/ru/diasoft/springHW/App.java
@@ -1,8 +1,11 @@
 package ru.diasoft.springHW;
 
+import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.SQLException;
 
 // url h2 консоли: http://localhost:8080/h2-console
 // url базы: jdbc:h2:mem:testdb
@@ -10,9 +13,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
 
+        Console.main(args);
         // try to commit in new branch
         context.close();
     }
